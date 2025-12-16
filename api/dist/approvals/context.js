215a2a600c314824c61ae524c3bed895c5b443c0
yasoo2 <86189296+yasoo2@users.noreply.github.com>
2025-12-16 11:23:03 +0300

    chore(git): stop tracking dist build outputs

--- api/dist/approvals/context.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.planContext = void 0;
-const map = new Map();
-exports.planContext = {
-    set(approvalId, ctx) { map.set(approvalId, ctx); },
-    get(approvalId) { return map.get(approvalId); },
-    delete(approvalId) { map.delete(approvalId); }
-};
