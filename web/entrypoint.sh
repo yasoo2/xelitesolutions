5d2aacadb7a3c6a7275964c380a6e32c76b48c9a
yasoo2 <86189296+yasoo2@users.noreply.github.com>
2026-01-21 03:24:02 +0300

    fix: Set production API URL in entrypoint.sh

--- web/entrypoint.sh
+++ web/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Default values
-API_URL=${VITE_API_URL:-"https://api.xelitesolutions.com"}
+API_URL=${VITE_API_URL:-"http://46.224.187.142:3000"}
 WS_URL=${VITE_WS_URL:-""}
 FEATURE_BROWSER_CHROME=${FEATURE_BROWSER_CHROME:-${VITE_FEATURE_BROWSER_CHROME:-"0"}}
 
